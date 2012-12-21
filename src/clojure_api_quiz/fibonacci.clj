12a234f40a8281f9a58bc11a95f3d8ce78a7670d
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2012-12-21 10:24:21 +0200

    Smart fibs.

--- src/clojure_api_quiz/fibonacci.clj
+++ src/clojure_api_quiz/fibonacci.clj
@@ -1,9 +1,19 @@
 (ns clojure-api-quiz.fibonacci)
 
-(defn fibs []
-  (defn fib-step [[a b]]
-    [b (+ a b)])
-  (map first (iterate fib-step [0N 1N])))
+(defn fib-iter [a b p q n]
+  (defn square [x] (* x x))
+  (cond
+    (= n 0) b
+    (even? n) (fib-iter a
+                        b
+                        (+ (square p) (square q))
+                        (+ (* 2 p q) (square q))
+                        (/ n 2))
+    :else (fib-iter (+ (* b q) (* a q) (* a p))
+                    (+ (* b p) (* a q))
+                    p
+                    q
+                    (- n 1))))
 
 (defn fib [n]
-  (nth (fibs) n))
+  (fib-iter 1N 0N 0N 1N n))
